5320ffb510f180664ae11f534c464be1f2e3d8ce
NIRUP <126146973+Nirup-L@users.noreply.github.com>
2026-07-15 18:31:24 +0530

    enabled chatapp

--- public/script.js
+++ public/script.js
@@ -13,8 +13,10 @@ window.addEventListener("DOMContentLoaded", () => {
   if (currentUser) {
     document.getElementById("loginPage").style.display = "none";
     document.getElementById("gallery").style.display = "none";
-    document.getElementById("notesPage").style.display = "block";
-    document.getElementById("chatApp").style.display = "none";
+    //document.getElementById("notesPage").style.display = "block";
+    //document.getElementById("chatApp").style.display = "none";
+    document.getElementById("chatApp").style.display = "block";
+    document.getElementById("notesPage").style.display = "none";
     document.getElementById("messageInput").value =
       localStorage.getItem("draftMessage") || "";
     adjustTextarea(document.getElementById("messageInput"));
@@ -92,8 +94,10 @@ function login() {
     localStorage.setItem("currentUser", currentUser);
     document.getElementById("loginPage").style.display = "none";
     document.getElementById("gallery").style.display = "none"; // Show gallery
-    document.getElementById("notesPage").style.display = "flex";
-    document.getElementById("chatApp").style.display = "none"; // Hide chat
+    document.getElementById("notesPage").style.display = "none";
+    document.getElementById("chatApp").style.display = "flex";
+    //document.getElementById("chatApp").style.display = "none"; // Hide chat
+    //document.getElementById("notesPage").style.display = "flex"
     document.getElementById("messageInput").value = "";
     localStorage.removeItem("draftMessage");
     const topLoader = document.getElementById("topLoader");
@@ -143,7 +147,7 @@ function logout() {
   document.getElementById("loginPage").style.display = "block";
   document.getElementById("gallery").style.display = "none"; // Show gallery
   document.getElementById("chatApp").style.display = "none"; // Hide chat
-  //document.getElementById("notesPage").style.display = "none";
+  document.getElementById("notesPage").style.display = "none";
 }
 
 window.addEventListener("beforeunload", () => {
