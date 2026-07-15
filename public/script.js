7f925e4c0d14646644630a96754493a4bb4d3a7b
NIRUP <126146973+Nirup-L@users.noreply.github.com>
2026-07-15 09:17:25 +0530

    Show chat app for logged-in users
    Updated chat application display logic to show chat when a user is logged in.

--- public/script.js
+++ public/script.js
@@ -14,7 +14,7 @@ window.addEventListener("DOMContentLoaded", () => {
     document.getElementById("loginPage").style.display = "none";
     document.getElementById("gallery").style.display = "none";
     //document.getElementById("notesPage").style.display = "block";
-    document.getElementById("chatApp").style.display = "none";
+    document.getElementById("chatApp").style.display = "block";
     document.getElementById("messageInput").value =
       localStorage.getItem("draftMessage") || "";
     adjustTextarea(document.getElementById("messageInput"));
@@ -77,7 +77,7 @@ function login() {
     document.getElementById("loginPage").style.display = "none";
     document.getElementById("gallery").style.display = "none"; // Show gallery
     //document.getElementById("notesPage").style.display = "flex";
-    document.getElementById("chatApp").style.display = "none"; // Hide chat
+    document.getElementById("chatApp").style.display = "flex; // Hide chat
     document.getElementById("messageInput").value = "";
     localStorage.removeItem("draftMessage");
     const topLoader = document.getElementById("topLoader");
