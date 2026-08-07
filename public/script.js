b1044d7c18fdc6d2b4b42bb98624f19c751153cd
NIRUP <126146973+Nirup-L@users.noreply.github.com>
2026-08-07 10:13:57 +0530

    Change chat page disable timeout duration

--- public/script.js
+++ public/script.js
@@ -45,7 +45,7 @@ function enableChatPage()
 function isChatPageEnable(flag)
 {
   if(flag==false){
-   let time = 0;
+   let time = 0.7;
     setTimeout(function() {
       disableChatPage();
     }, time*1000);
